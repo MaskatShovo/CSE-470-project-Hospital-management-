ded0c1bfa65f7e918174049626951c8004556ff8
MaskatShovo <maswashovo3500@gmail.com>
2024-12-17 23:05:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from controllers.main_controller import main_routes
+from models.user_model import init_db
+app = Flask(__name__, template_folder="views")
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "home4321"
+app.config["MYSQL_DB"] = "hospital_management"
 
-app = Flask(__name__,template_folder="views")
 
-# Register Blueprints
+init_db(app)
+
 app.register_blueprint(main_routes)
 
 if __name__ == '__main__':
